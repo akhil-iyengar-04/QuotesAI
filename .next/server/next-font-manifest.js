d50982fb668f1ec7c821dbac8aacb426d7e61a2f
Akhil Iyengar <akhiliyengar2004@gmail.com>
2024-12-06 00:55:45 -0500

    s

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/kanshruti/Documents/GitHub/QuotesAI/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"],"/Users/kanshruti/Documents/GitHub/QuotesAI/app/(marketing)/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"E4QCzZfOROTvlUdw9mnU4gQxG9jAdjgjpisb5Xt7F5U=\"}"
